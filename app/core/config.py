0cefe877e7f6dd3f799f7da9ed38f6c11c85bbef
p.nikitin <p.nikitin@itorummr.com>
2025-11-29 06:13:07 +0300

    check small model

--- app/core/config.py
+++ app/core/config.py
@@ -3,4 +3,4 @@ import os
 # Имя модели Whisper для использования в транскрипции.
 # Варианты: "tiny", "base", "small", "medium", "large"
 # Подробнее: https://github.com/openai/whisper
-WHISPER_MODEL = os.getenv("WHISPER_MODEL", "base")
+WHISPER_MODEL = os.getenv("WHISPER_MODEL", "small")
